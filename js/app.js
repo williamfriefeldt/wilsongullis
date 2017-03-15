07595dfa90b89039c1bfcb2716870ebe5164c1d7
philip gullberg <philip gullberg>
2017-03-15 16:41:35 +0100

    localhost upload

--- js/app.js
+++ js/app.js
@@ -3,10 +3,13 @@ var WGWebsite = angular.module('wilsongulliswebsite',['ngRoute','ngResource']);
 WGWebsite.config(['$routeProvider',
 	function($routeProvider){
 		$routeProvider.
-		when('/', {
+		when('/home', {
 			templateUrl: 'partials/home.html'
 		}).
+		when('/blog', {
+			templateUrl: 'partials/blog.html'
+		}).
 		otherwise({
-        redirectTo: '/home.html'
+        redirectTo: '/home'
       });
 }]);
